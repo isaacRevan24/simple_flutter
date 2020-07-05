533fa14262aee0d85b974c826547341558e8cbc3
Isaac Atencio <isaacatencio@pop-os.localdomain>
2020-07-05 15:02:03 -0500

    Manager list in project info

--- lib/presentation/pages/individual_project/local_widgets/project_info_managers_presentation.dart
+++ lib/presentation/pages/individual_project/local_widgets/project_info_managers_presentation.dart
@@ -1,23 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 class ManagerInfoPresentation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.cyan,
+      // color: Colors.cyan,
       child: Column(
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-            alignment: Alignment.topLeft,
-            child: Text(
-              'Manager',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
-              ),
-            ),
+          _TitleText(),
+          _ManagerListCards(),
+        ],
+      ),
+    );
+  }
+}
+
+class _TitleText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 25, top: 20, right: 25),
+      alignment: Alignment.topLeft,
+      child: Text(
+        'Manager',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+        ),
+      ),
+    );
+  }
+}
+
+class _ManagerListCards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      alignment: Alignment.topLeft,
+      child: SizedBox(
+        height: 135.0,
+        child: ListView.builder(
+          physics: ClampingScrollPhysics(),
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          // max 5 manager
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) => Card(
+            child: _ManagerCard(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ManagerCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FlutterLogo(size: 70),
+          ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 100),
+            child: Text('Manager Name'),
           ),
         ],
       ),
