3d812e6456f2c1b50d46d2894302ef86414d219c
Isaac Atencio <isaacatencio@pop-os.localdomain>
2020-07-05 21:22:20 -0500

    App bar Project documentation

--- lib/presentation/pages/projects/local_widgets/project_app_bar.dart
+++ lib/presentation/pages/projects/local_widgets/project_app_bar.dart
@@ -4,33 +4,48 @@ AppBar buildAppBar() {
   return AppBar(
     backgroundColor: Colors.white,
     elevation: 0,
-    bottom: TabBar(
-      labelStyle: TextStyle(color: Colors.black),
-      unselectedLabelColor: Colors.grey,
-      labelColor: Colors.black,
-      tabs: <Widget>[
-        Tab(
-          text: 'Active',
-        ),
-        Tab(
-          text: 'Finished',
-        ),
-      ],
-    ),
-    title: Text(
-      'Your projects',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
+    bottom: tabBar(),
+    title: projectAppBarTitle(),
+    actions: projectAppBarActionButton(),
+  );
+}
+
+/// Project app bar buttons
+List<Widget> projectAppBarActionButton() {
+  return <Widget>[
+    IconButton(
+      icon: Icon(
+        Icons.filter_list,
         color: Colors.black,
       ),
+      onPressed: () {},
     ),
-    actions: <Widget>[
-      IconButton(
-        icon: Icon(
-          Icons.filter_list,
-          color: Colors.black,
-        ),
-        onPressed: () {},
+  ];
+}
+
+/// Project app bar title
+Text projectAppBarTitle() {
+  return Text(
+    'Your projects',
+    style: TextStyle(
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+  );
+}
+
+/// Project app bar tabs
+TabBar tabBar() {
+  return TabBar(
+    labelStyle: TextStyle(color: Colors.black),
+    unselectedLabelColor: Colors.grey,
+    labelColor: Colors.black,
+    tabs: <Widget>[
+      Tab(
+        text: 'Active',
+      ),
+      Tab(
+        text: 'Finished',
       ),
     ],
   );
