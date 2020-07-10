16d39bb20430950bd9273d4a20bc534f54b9ad0b
Isaac Atencio <isaacatencio@pop-os.localdomain>
2020-07-10 11:14:55 -0500

    Agregada section name

--- lib/presentation/pages/individual_project/local_widgets/project_structure/hero_section_info.dart
+++ lib/presentation/pages/individual_project/local_widgets/project_structure/hero_section_info.dart
@@ -1,55 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class HeroSectionInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back_ios,
-              color: Colors.black,
-            ),
-            onPressed: () => Navigator.of(context).pop(),
+        appBar: _sectionDetailAppbar(context),
+        body: _sectionDetails(),
+      ),
+    );
+  }
+
+  Container _sectionDetails() {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.8,
+          heightFactor: 0.75,
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 2,
+                child: Container(
+                  color: Colors.white,
+                  child: LinearPercentIndicator(
+                    lineHeight: 20.0,
+                    percent: 0.8,
+                    center: Text("80.0%"),
+                    linearStrokeCap: LinearStrokeCap.roundAll,
+                    progressColor: Colors.green,
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 8,
+                child: Container(
+                  color: Colors.blue,
+                  child: Column(
+                    children: <Widget>[
+                      Center(
+                        child: Text('data'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-          title: Text(
-            'Section detail',
-            style: TextStyle(color: Colors.black),
-          ),
-          backgroundColor: Colors.white,
-        ),
-        body: Container(
-          color: Colors.white,
-          child: _sectionDetails(),
         ),
       ),
     );
   }
 
-  Center _sectionDetails() {
-    return Center(
-      child: FractionallySizedBox(
-        widthFactor: 0.8,
-        heightFactor: 0.75,
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              flex: 1,
-              child: Container(
-                color: Colors.brown,
-              ),
-            ),
-            Flexible(
-              flex: 8,
-              child: Container(
-                color: Colors.blue,
-              ),
-            ),
-          ],
+  AppBar _sectionDetailAppbar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
         ),
+        onPressed: () => Navigator.of(context).pop(),
       ),
+      title: Text(
+        'Section name',
+        style: TextStyle(color: Colors.black),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
     );
   }
 }
