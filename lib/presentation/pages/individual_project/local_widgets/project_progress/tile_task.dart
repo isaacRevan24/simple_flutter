0e275bef8a195d5aa0affcf4715dfc241b7e3449
Isaac Atencio <isaacatencio@pop-os.localdomain>
2020-07-23 23:03:32 -0500

    agregado bottom task desription a tarea

--- lib/presentation/pages/individual_project/local_widgets/project_progress/tile_task.dart
+++ lib/presentation/pages/individual_project/local_widgets/project_progress/tile_task.dart
@@ -2,15 +2,40 @@ import 'package:flutter/material.dart';
 
 ///
 class TileTask extends StatelessWidget {
+  final String userName = 'Severiano Atencio';
+  final String date = '24/4/2020';
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 8.0),
-      child: Card(
-        child: Container(
-          child: Center(
-            child: Text("Place holder task ....."),
-          ),
+      padding: const EdgeInsets.only(bottom: 12),
+      child: Column(
+        children: <Widget>[
+          _taskContent(),
+          _bottomTaskDescription(),
+        ],
+      ),
+    );
+  }
+
+  Container _bottomTaskDescription() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(userName),
+          Text(date),
+        ],
+      ),
+    );
+  }
+
+  Card _taskContent() {
+    return Card(
+      child: Container(
+        child: Center(
+          child: Text("Place holder task ....."),
         ),
       ),
     );
